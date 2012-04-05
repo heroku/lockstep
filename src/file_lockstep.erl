587bec7869deab90a6ad69bb5dea4f14fc7308ce
Geoff Cant <nem@erlang.geek.nz>
2012-04-04 17:01:50 -0700

    Count number of records, report timing on success.

--- src/file_lockstep.erl
+++ src/file_lockstep.erl
@@ -9,13 +9,21 @@
 -export([run/2]).
 
 %% for timer:tc
--export([foreach_line/2]).
+-export([foreach_line/3]).
 
 run(Mod, File) ->
     {ok, F} = file:open(File, [binary, read_ahead, raw]),
     try
         {ok, _, MState} = cb_init(Mod),
-        timer:tc(?MODULE, foreach_line, [{Mod, MState}, F])
+        case timer:tc(?MODULE, foreach_line, [{Mod, MState}, F, 0]) of
+            {Time, {ok, State, Count}} ->
+                {ok, [{state, State},
+                      {records, Count},
+                      {total, Time},
+                      {time_per_record, Time / Count}]};
+            Else ->
+                {error, Else}
+        end
     after
         file:close(F)
     end.
@@ -23,22 +31,22 @@ run(Mod, File) ->
 cb_init(Mod) ->
     Mod:init([]).
 
-foreach_line(MS, F) ->
-    foreach_line(MS, F, file:read_line(F)).
+foreach_line(MS, F, Cnt) ->
+    foreach_line(MS, F, file:read_line(F), Cnt).
 
-foreach_line({Callback, CbState}, F, {ok, Line}) ->
+foreach_line({Callback, CbState}, F, {ok, Line}, Cnt) ->
     case catch mochijson2:decode(Line) of
         {struct, Props} ->
             case catch Callback:handle_msg(Props, CbState) of
                 {noreply, CbState1} ->
                     NewMS = {Callback, CbState1},
-                    foreach_line(NewMS, F);
+                    foreach_line(NewMS, F, Cnt+1);
                 {stop, Reason, CbState1} ->
-                    {Reason, CbState1};
+                    {Reason, CbState1, Cnt};
                 {'EXIT', Err} ->
-                    {Err, CbState}
+                    {Err, CbState, Cnt}
             end
     end;
-foreach_line(MS, _F, eof) ->
-    MS.
+foreach_line(MS, _F, eof, Cnt) ->
+    {ok, MS, Cnt}.
 
